76939779c1b86a6c192f350b89b9da66da59f882
Nicolas COCHIN <github@account.rocks>
2021-03-20 16:01:43 +0100

    Create setup_shell.sh

---
+++ setup_shell.sh
@@ -0,0 +1,33 @@
+echo "install oh my zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Download fonts (https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k) into ~/.local/share/fonts"
+FONT_DIR=~/.local/share/fonts 
+mkdir -p $FONT_DIR
+curl -fsSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o "${FONT_DIR}/MesloLGS NF Regular.ttf"
+curl -fsSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -o "${FONT_DIR}/MesloLGS NF Bold.ttf"
+curl -fsSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf  -o "${FONT_DIR}/MesloLGS NF Italic.ttf"
+curl -fsSL https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf o "${FONT_DIR}/MesloLGS NF Bold Italic.ttf"
+echo "Setup gnome terminal to use the new font"
+read
+echo "install powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "install syntax highlighted"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "install oh my zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Download fonts (https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k) into ~/.local/share/fonts"
+read
+echo "Setup gnome terminal to use the new font"
+read
+echo "install powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "install syntax highlighted"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "install fzf"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+echo "copy config files"
+for i in .gitconfig .p10k.zsh .tmux.conf .zshrc; do
+  echo "Setting $i"
+  curl https://raw.githubusercontent.com/nicolascochin/setup/main/$i -o $i
+end
