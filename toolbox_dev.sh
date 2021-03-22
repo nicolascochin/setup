4cef964cfebadc31c14481b040c26c9ab03877cd
Nicolas COCHIN <github@account.rocks>
2021-03-22 14:04:04 +0100

    Update toolbox_dev.sh

--- toolbox_dev.sh
+++ toolbox_dev.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env bash
+#/bin/bash
 
-echo "Setup a new dev toolbox"
+while
+  echo -n "Please enter the SSH port to setup "
+  read SSH_PORT
+  ! echo ${SSH_PORT} | egrep -q "^[0-9]+$"
+do true; done
+
+echo
+echo "Setup a dev toolbox"
 echo "install ZSH"
 sudo dnf install -qy zsh
 echo "install development Tools"
@@ -13,24 +20,11 @@ echo "install nodenv"
 curl -fsSL https://raw.githubusercontent.com/nodenv/nodenv-installer/master/bin/nodenv-installer | bash
 git clone -q https://github.com/mattberther/zsh-nodenv ~/.oh-my-zsh/custom/plugins/zsh-nodenv
 
-#echo "install vscode"
-#sudo dnf -qy install libX11-devel
-#sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-#sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-##sudo dnf check-update
-#sudo dnf install -qy code
-
 echo "install lib PG"
 sudo dnf install -qy libpq-devel
 
 echo "install Heroku cli"
 curl -s https://cli-assets.heroku.com/install.sh | sh
-
-#echo "install code extensions"
-#for i in $(curl -s https://raw.githubusercontent.com/nicolascochin/setup/main/toolbox/code_extensions.txt); do 
-#  echo "Install $i..."
-#  code --install-extension  $i
-#done
 
 echo "Setup ssh server"
 sudo dnf -qy install openssh-server
@@ -39,17 +33,15 @@ sudo /usr/libexec/openssh/sshd-keygen ecdsa
 sudo /usr/libexec/openssh/sshd-keygen ed25519
 sudo rm /etc/ssh/sshd_config
 sudo touch /etc/ssh/sshd_config
-echo "Port 2222                 # Prevent conflicts with other SSH servers" | sudo tee -a /etc/ssh/sshd_config
+echo "Port ${SSH_PORT}          # Prevent conflicts with other SSH servers" | sudo tee -a /etc/ssh/sshd_config
 echo "ListenAddress localhost   # Don’t allow remote connections" | sudo tee -a /etc/ssh/sshd_config
 echo "PermitEmptyPasswords yes  # Containers lack passwords by default" | sudo tee -a /etc/ssh/sshd_config
 
-echo "Add this line to the zshrc" 
+echo "Add this line to the zshrc"
 echo "toolbox run -c dev sudo /usr/sbin/sshd"
 echo
 echo "Add the following lines to ~/.ssh/config"
 echo
 echo "Host toolbox"
 echo "    HostName localhost"
-echo "    Port 2222"
-#echo "install netlify cli"
-#npm install netlify-cli -g
+echo "    Port ${SSH_PORT}"
