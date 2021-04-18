ec02b06ade77cd7d9c0f4bdfe3c4d698d32326a4
Nicolas COCHIN <github@account.rocks>
2021-04-18 23:09:47 +0200

    Update silverblue.sh

--- silverblue.sh
+++ silverblue.sh
@@ -2,7 +2,7 @@ echo "add rpm fusion repos"
 sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 reboot
 echo "install chromium, bat & zsh"
-sudo rpm-ostree install mozilla-openh264 chromium-freeworld bat zsh
+sudo rpm-ostree install mozilla-openh264 chromium-freeworld bat zsh fprintd fprintd-pam 
 echo "set ZSH as default shell"
 sudo usermod --shell /bin/zsh nico
 reboot
